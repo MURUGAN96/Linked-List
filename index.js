dfc9451e203807dc38f8bc68a1015fd673a7f489
MURUGAN96 <1murugan23@gmail.com>
2022-08-25 21:46:37 +0530

    Linked list - Append, Prepend, Fetch

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 // Import stylesheets
 import './style.css';
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = { value: value, next: null };
+    if (this.tail) this.tail.next = newNode;
+    this.tail = newNode;
+    if (!this.head) this.head = newNode;
+  }
+
+  prepend(value) {
+    const firstNode = { value: value, next: this.head };
+    this.head = firstNode;
+    if (!this.tail) this.tail = firstNode;
+  }
+
+  formatArray() {
+    const elements = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      elements.push(currentNode);
+      currentNode = currentNode.next;
+    }
+    return elements;
+  }
+}
+
+const myList = new LinkedList();
+// myList.append(1);
+// myList.append('Linked List');
+myList.prepend('first List');
+console.log(myList.formatArray());
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Linked List</h1>`;
